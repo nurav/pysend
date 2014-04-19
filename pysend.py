bfe5c899546ce8fadac9c2743035cad83acf9092
nurav <varunj.1011@gmail.com>
2014-04-19 23:27:43 +0530

    make source prettier

--- pysend.py
+++ pysend.py
@@ -1,114 +1,104 @@
 
-import socket,sys,os
+import socket
+import sys
+import os
 from os.path import expanduser, join
+
+
+MAX=65536
 PORT=1060
+BASE_PATH='~/Downloads/pysend/'
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
 def findfreename(name,folder):
     i=1
-    while os.path.exists(join(expanduser('~/Downloads/pysend'),'.'.join(name.split('.')[:-1])+'(%d)'%i+'.'+name.split('.')[-1])):
+    while os.path.exists(join(expanduser(BASE_PATH),'.'.join(name.split('.')[:-1])+'(%d)'%i+'.'+name.split('.')[-1])):
         i+=1
     return '.'.join(name.split('.')[:-1])+'(%d)'%i+'.'+name.split('.')[-1]
         
 
 def recvall(sock, length):
-    
     data = b''
     name=''
-
     flag = 0
     while True:
-        
-            
         more=sock.recv(1)
         if more=='\r':
-            
             flag+=1
             length=len(data)+int(data)
             l2=length
             print 'the file is %d bytes long' %length
-            
             break
         data+=more
     while True:
-        
-            
         more=sock.recv(1)
-        
         if more=='\r':
             print 'the name is %s' %name
-            
             length=len(data)+int(data)
-            
-            recvall2(sock,l2,name)
+            recvmessage(sock,l2,name)
             break
         name+=more
-        
-    
     return l2
-def recvall2(sock,length,name):
+    
+    
+def recvmessage(sock,length,name):
     f=None
-    if os.path.exists(join(expanduser('~/Downloads/pysend/'),name)):
+    if os.path.exists(join(expanduser(BASE_PATH),name)):
         c=raw_input('overwrite? (Y/n) : ')
         if c=='Y' or c=='y':
-            f=open(join(expanduser('~/Downloads/pysend/'),name),'a')
+            f=open(join(expanduser(BASE_PATH),name),'a')
         else :
-            f=open(join(expanduser('~/Downloads/pysend/'),findfreename(name,expanduser('~/Downloads/pysend/'))),'a')
+            f=open(join(expanduser(BASE_PATH),findfreename(name,expanduser(BASE_PATH))),'a')
     else:
-        f=open(join(expanduser('~/Downloads/pysend/'),name),'a')
-    
+        f=open(join(expanduser(BASE_PATH),name),'a')
     data = b''
     donel=0
     flag = 0
     while donel < length:
-        more = sock.recv(65536)
+        more = sock.recv(MAX)
         if not more:
             break
         f.write(more)
         donel+=len(more)
         sys.stdout.flush()
         sys.stdout.write("\r%d%% done" % (donel*100/(length-10)))
+        
+        
 if len(sys.argv)==2 and sys.argv[1]=='receive':
     print 'press ctrl+c to stop receiving files'
-    if not os.path.isdir(expanduser('~/Downloads/pysend')):
-        os.makedirs(expanduser('~/Downloads/pysend'))
+    if not os.path.isdir(expanduser(BASE_PATH)):
+        os.makedirs(expanduser(BASE_PATH))
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('',1060))
     s.listen(1)
     while True:
-        
         sc,sockname=s.accept()
-        
-        
-        
-        data=recvall(sc,2**15)
+        print '\nnew connection received'
+        data=recvall(sc,MAX)
         sys.stdout.flush()
         print("\nfile received")
-        
         sc.close()
 elif len(sys.argv)==4 and sys.argv[1] == 'send':
     s.connect((sys.argv[2], PORT))
-    print('Client has been assigned socket name', s.getsockname())
-    f=open(sys.argv[3],'rb')
-    size=os.path.getsize(sys.argv[3])
-    
-    s.send(str(os.path.getsize(sys.argv[3]))+'\r'+sys.argv[3].split('/')[-1]+'\r')
+    print 'sending to ' + sys.argv[2] + ':' + str(PORT)
+    path=sys.argv[3]
+    f=open(path,'rb')
+    size=os.path.getsize(path)
+    s.send(str(size)+'\r'+path.split('/')[-1]+'\r')
     i=0
     while True:
-        m=f.read(65536)
+        m=f.read(MAX)
         if m=='':
             break
         s.sendall(m)
-        i+=1
-        if not size<65536:
+        i += 1
+        if not size<MAX:
             sys.stdout.flush()
-        
-            sys.stdout.write('\r%d%% sent' %(65536*i*100/size))
+            sys.stdout.write('\r%d%% sent' %(MAX*i*100/size))
         else:
             print 'done'
             s.close()
             exit(0)
-        
-    
     print ' '  
     s.close()
 else :
